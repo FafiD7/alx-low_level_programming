166f367f3b46fcea3f9b6c5f2b1fbc12957a4acc
fasika <fafidtb7@gmail.com>
2023-02-21 14:23:04 -0800

    print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main-A function that prints ten times the alphabet 
+ *
+ * Return: x10 a-z (Success)
+ */
+void print_alphabet_x10(void);
+{
+	char i, j;
+
+	j = 0;
+	while (j < 10)
+	{
+		for (i = 'a' ; i <= 'z' ; i++)
+		{
+			_putchar (i);
+		}
+		j++;
+			_putchar('\n');
+	}
+}
+
