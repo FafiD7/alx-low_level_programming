a0caca1eae637a1fb10fe2baaf84edec23014b55
fasika <fafidtb7@gmail.com>
2023-02-21 11:45:39 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <main.h>
+/**
+ * main - Print putchar alphabets
+ *  Return: Always 0
+ */
+
+int main(void)
+{
+	_putchar('-');
+	-putchar('p');
+	-putchar('u');
+	-putchar('t');
+	-putchar('c');
+	-putchar('h');
+	-putchar('a');
+	-putchar('r');
+	-putchar("\n");
+	return (0);
+}
