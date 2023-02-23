737a2cf32d271fe378bee1416f482e2f1a297ea0
fasika <fafidtb7@gmail.com>
2023-02-22 22:36:33 -0800

    checking

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+psitive
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* positive_or_negative - tests function for positive or negative
+* @i: the number to be tasted
+* Return: 0
+*/
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative", i);
+	else
+		printf("%d is positive", i);
+}
