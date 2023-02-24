e744ba88c104ad92257fc9aa27be3207210ef156
fasika <fafidtb7@gmail.com>
2023-02-24 12:24:22 -0800

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - A function that prints the number, from 0 to 9
+ *
+ * Return: always 0
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+		for (c = 0; c < 10; c++)
+		{
+			putchar(c + '0');
+			putchar('\n');
+		}
+
+}
