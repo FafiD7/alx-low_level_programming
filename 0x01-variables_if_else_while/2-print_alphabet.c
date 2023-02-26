80cf5144da526ec7a9a0a4c70f28261487240697
fasika <fafidtb7@gmail.com>
2023-02-26 07:46:16 -0800

    lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - A function that print the alphabet in lowercase
+ *
+ * Retuen: Always 0
+ */
+
+int main(void)
+{
+	int a;
+
+	if (a >= 97 && a <= 122)
+	{
+		putchar('a');
+		putchar('\n');
+	}
+	return (0);
+}
