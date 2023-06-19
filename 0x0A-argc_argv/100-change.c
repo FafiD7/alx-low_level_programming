33b1022160047cc776e8d26c9100c601ccb23ef0
FafiD7 <fafidtb7@gmail.com>
2023-06-19 14:25:18 -0700

    macro

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include "main.h"
+/**
+ * main - a program that prints the minimum number of coins.
+ * @argv: an array of argument
+ * @argc: a number of argument
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+
